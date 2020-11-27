79114c8910acf23971a33da6246df3a0b1f25072
JackL1995 <jlenny@colostate.edu>
2020-11-27 14:41:59 -0700

    13.2 11/27 add router and components

---
+++ src/components/WorkOrders.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Work Order</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/routes.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from './components/Home.vue';
+import Customer from './components/Customer.vue';
+import Account from './components/Account.vue';
+import Login from './components/Login.vue';
+import NotFound from './components/NotFound.vue';
+import SignUp from './components/SignUp.vue';
+import Vehicles from './components/Vehicles.vue';
+import VehicleDetails from './components/VehicleDetails.vue';
+import WorkCreate from './components/WorkCreate.vue';
+import WorkOrders from './components/WorkOrders.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/', component: Home},
+    {path: '/account', component: Account},
+    {path: '/signin', component: Login},
+    {path: '/vehicles', component: Vehicles},
+    {path: '/vehicles/:pk', component: VehicleDetails,
+        children: [
+            {path: 'workorder', component: WorkCreate}
+        ]},
+    {path: '/signup', component: SignUp},
+    {path: '/:invalidroute(.*)', component: NotFound}
+]
+
+export default new VueRouter({mode: 'history', routes:routes})
